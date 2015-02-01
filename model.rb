0737f0f43febf935524a6f5f1a8bb293b9500b00
Helio Oliveira <hheelliioo@live.com>
2015-02-01 14:44:05 -0200

    Config bcrypt

--- model.rb
+++ model.rb
@@ -1,11 +1,23 @@
+require 'bcrypt'
 DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db.sqlite")
 
 class User
   include DataMapper::Resource
+  include BCrypt
 
   property :id, Serial, key: true
   property :email, String
-  property :password, String
+  property :failed_login_attempts, Integer, default: 0
+  property :blocked_user, Boolean, default: false
+  property :password, BCryptHash
+
+  def authenticate(attempted_password)
+    if self.password == attempted_password
+      true
+    else
+      false
+    end
+  end
 end
 
 DataMapper.finalize
