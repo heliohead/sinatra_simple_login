04fdee516a7074c0ec9f69b43e3fb14286d05154
Helio Oliveira <hheelliioo@live.com>
2015-02-01 14:07:38 -0200

    Config routes with warden

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-sqlite-adapter'
+gem 'dm-adjust'
 gem 'warden'
+gem "rack-flash3" , '1.0.1', require: 'rack/flash'
--- routes.rb
+++ routes.rb
@@ -1,19 +1,43 @@
 module App
   class SinatraSimpleLogin < Sinatra::Base
-    get '/' do
-      erb :index
-    end
+  get '/' do
+    erb :index
+  end
 
-    get '/login' do
-      erb :login
-    end
+  get '/login' do
+    erb :login
+  end
 
-    get '/logout' do
+  post '/login' do
+    env['warden'].authenticate!
+
+    flash[:success] = env['warden'].message
+
+    if session[:return_to].nil?
       redirect '/'
+    else
+      redirect session[:return_to]
     end
+  end
 
-    get '/protected' do
-      erb :protected
-    end
+  get '/logout' do
+    env['warden'].raw_session.inspect
+    env['warden'].logout
+    flash[:success] = 'Successfully logged out'
+    redirect '/'
+  end
+
+  post '/unauthenticated' do
+    session[:return_to] = env['warden.options'][:attempted_path] if session[:return_to].nil?
+    puts env['warden.options'][:attempted_path]
+    puts env['warden']
+    flash[:error] = env['warden'].message || "You must log in"
+    redirect '/login'
+  end
+
+  get '/protected' do
+    env['warden'].authenticate!
+
+    erb :protected
   end
 end
